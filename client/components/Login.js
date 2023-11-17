8e9213553b84abf530a4273a2e86a0d43b974385
Damian Mirowski <damian_mirowski@wp.pl>
2023-11-17 22:13:43 +0100

    Prepared Login component.

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    const [user, setUser] = useState(null);
+
+    const login = () => {}
+
+    return (
+        <div className="login">
+            <label>Login</label>
+            <input
+                placeholder="Username"
+                onChange={(event) => {
+                    setUser({ ...user, username: event.target.value })
+                }}
+            />
+            <input
+                placeholder="Password"
+                onChange={(event) => {
+                    setUser({ ...user, password: event.target.value })
+                }}
+            />
+            <button onClick={login}>Login</button>
+        </div>
+    )
 }
 
 export default Login
