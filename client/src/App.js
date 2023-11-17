a7c5c93a6c9b6dd5472944397555145b143a850d
Damian Mirowski <damian_mirowski@wp.pl>
2023-11-17 22:35:37 +0100

    Necessary fixes and call Login and SignUp in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <SignUp/>
+      <Login/>
+    </div>
   );
 }
 
