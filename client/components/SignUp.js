da13f1f43fa00a35aa51c2a46329d75406337a72
Damian Mirowski <damian_mirowski@wp.pl>
2023-11-17 22:07:37 +0100

    Prepare SignUp component.

---
+++ client/components/SignUp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const [user, setUser] = useState(null);
+
+const signUp = () => {}
+
+function SignUp() {
+  return (
+    <div className="signUp">
+        <label>SignUp</label>
+        <input
+            placeholder="First name"
+            onChange={(event) => {
+                setUser({ ...user, firstName: event.target.value })
+            }}
+        />
+        <input
+            placeholder="Last name"
+            onChange={(event) => {
+                setUser({ ...user, lastName: event.target.value })
+            }}
+        />
+        <input
+            placeholder="Username"
+            onChange={(event) => {
+                setUser({ ...user, username: event.target.value })
+            }}
+        />
+        <input
+            placeholder="Password"
+            onChange={(event) => {
+                setUser({ ...user, password: event.target.value })
+            }}
+        />
+        <button onClick={signUp}>Sign Up </button>
+    </div>
+  )
+}
+
+export default SignUp
